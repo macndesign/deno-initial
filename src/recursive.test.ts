2bfaa5ffdc85e5cadb1ddaf9c6b44d0f79a4822a
marionvmware <marion@vmware.com>
2022-09-22 09:20:55 -0300

    add arr sum, length and higher algs

--- src/recursive.test.ts
+++ src/recursive.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.145.0/testing/asserts.ts"
-import { factorial, countdown } from './recursive.ts'
+import { factorial, countdown, sum, length, higher } from './recursive.ts'
 
 Deno.test({
   name: "Recursive",
@@ -8,6 +8,7 @@ Deno.test({
       const fact5 = factorial(5)
       assertEquals(fact5, 120)
     })
+
     await t.step('Countdown', () => {
       const arr: number[] = []
       countdown(10, arr)
@@ -15,6 +16,31 @@ Deno.test({
         10, 9, 8, 7, 6,
         5, 4, 3, 2, 1
       ])
+    })
+
+    await t.step('Sum', () => {
+      const arr1: number[] = [1, 2, 3, 4, 5]
+      const arr2: number[] = [7, 3, 5, 2, 4]
+      assertEquals(sum(arr1), 15)
+      assertEquals(sum(arr2), 21)
+    })
+
+    await t.step('Length', () => {
+      const arr1: number[] = [7, 3, 5, 2, 4]
+      const arr2: string[] = ['a', 'b', 'c', 'd']
+      assertEquals(length(arr1), 5)
+      assertEquals(length(arr2), 4)
+    })
+
+    await t.step('Higher', () => {
+      const arr1: number[] = [3, 12, 5, 28, 9]
+      const arr2: number[] = [3, 55, 5, 28, 9]
+      const arr3: number[] = [3, 55, 5, 88, 99]
+      const arr4: string[] = ['f', 't', 'g', 'z', 'a']
+      assertEquals(higher(arr1), 28)
+      assertEquals(higher(arr2), 55)
+      assertEquals(higher(arr3), 99)
+      assertEquals(higher(arr4), 'z')
     })
   }
 })
--- src/recursive.ts
+++ src/recursive.ts
@@ -12,3 +12,28 @@ export function countdown(num: number, arr: number[]): null {
   }
   return countdown(num - 1, arr)
 }
+
+export function sum<T>(arr: number[]): number {
+  if (arr.length === 0) {
+    return 0
+  }
+  const num = arr.pop() as number
+  return num + sum(arr)
+}
+
+export function length<T>(arr: T[]): number {
+  if (arr.length === 0) {
+    return 0
+  }
+  arr.pop()
+  return 1 + length(arr)
+}
+
+export function higher<T>(arr: T[]): T {
+  if (arr.length === 2) {
+    return arr[0] > arr[1] ? arr[0] : arr[1]
+  }
+  const newArr = arr.slice(1)
+  const subMax = higher(newArr)
+  return arr[0] > subMax ? arr[0] : subMax
+}
